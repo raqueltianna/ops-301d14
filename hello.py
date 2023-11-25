60dca88d0d0d972c906ad8e297f5a461454b3622
Tianna Farrow <raqueltianna@outlook.com>
2023-11-25 12:21:33 -0500

    CREATED HELLO.PY

---
+++ hello.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Script Name:                  hello.py
+# Author:                       Tianna Farrow
+# Date of latest revision:      11/25/23
+# Purpose:                      Demonstrating able to connect to python through my lab pc. 
+# Execution:                    python3 hello.py 
+# Additional Resources:         https://realpython.com/python-print/#calling-print
+
+print("Hello World")
+#Output: Hello World!

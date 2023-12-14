311a3531b58bedf9106b46f3098829ca4d253e76
Tianna Farrow <raqueltianna@outlook.com>
2023-12-14 00:34:11 -0500

    added ops challenege 13

---
+++ adautomation.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3 
+
+# Script Name:                          adautomation.sh
+# Author name:                          Tianna Farrow
+# Date of latest revision:              12/13/23
+# Purpose:                              Create a powershell script to add a new user to AD. 
+# Execution:                            bash adautomation.sh
+# Additional Resources:                 https://lazyadmin.nl/powershell/add-user-to-group-add-adgroupmember/; https://lazyadmin.nl/powershell/add-user-to-group-add-adgroupmember/; 
+ 
+ # User Info
+$firstName = "Franz"
+$lastName = "Ferdinand"
+$username = "ferdi"
+$jobTitle = "TPS Reporting Lead"
+$department = "TPS Department"
+$email = "ferdi@GlobeXpower.com"
+$office = "Springfield, OR"
+
+# User in AD
+New-ADUser -SamAccountName $username -GivenName $firstName -Surname $lastName `
+    -DisplayName "$firstName $lastName" -Title $jobTitle -Department $department `
+    -EmailAddress $email -Office $office -Enabled $true `
+    -UserPrincipalName "$username@yourdomain.com" -AccountPassword (ConvertTo-SecureString "P@ssw0rd" -AsPlainText -Force)
+
+# Output
+Write-Host "User $username added successfully."
+
+# Remove-ADUser -Identity $username -Confirm:$false (if you need to remove user.)
